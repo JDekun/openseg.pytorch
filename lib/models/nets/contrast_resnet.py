6360ff57e185a4e6366dc92ea85f4e773e724c92
JDekun <jurry_k@outlook.com>
2023-05-08 15:52:56 +0800

    resnet_ocr_asp0_mep_in

--- lib/models/nets/contrast_resnet.py
+++ lib/models/nets/contrast_resnet.py
@@ -6,6 +6,60 @@ from torch.nn import functional as F
 from lib.models.backbones.backbone_selector import BackboneSelector
 from lib.models.tools.module_helper import ModuleHelper
 
+class RES_OCR_ASP_0_MEP_IN(nn.Module):
+
+    def __init__(self, configer):
+        self.inplanes = 128
+        super(RES_OCR_ASP_0_MEP_IN, self).__init__()
+        self.configer = configer
+        self.num_classes = self.configer.get("data", "num_classes")
+        self.backbone = BackboneSelector(configer).get_backbone()
+
+        # extra added layers
+        if "wide_resnet38" in self.configer.get("network", "backbone"):
+            in_channels = [2048, 4096]
+        else:
+            in_channels = [1024, 2048]
+
+        # Mep
+        from lib.models.nets.contrast_asp import OCR_ASP_0_MEP_IN
+        self.ocr_asp_head = OCR_ASP_0_MEP_IN(
+            configer=configer,
+            features=2048,
+            hidden_features=256,
+            out_features=256,
+            num_classes=self.num_classes,
+            bn_type=self.configer.get("network", "bn_type"),
+        )
+
+        self.head = nn.Conv2d(
+            256, self.num_classes, kernel_size=1, stride=1, padding=0, bias=True
+        )
+        self.dsn_head = nn.Sequential(
+            nn.Conv2d(in_channels[0], 512, kernel_size=3, stride=1, padding=1),
+            ModuleHelper.BNReLU(512, bn_type=self.configer.get("network", "bn_type")),
+            nn.Dropout2d(0.1),
+            nn.Conv2d(
+                512, self.num_classes, kernel_size=1, stride=1, padding=0, bias=True
+            ),
+        )
+
+    def forward(self, x_):
+        x = self.backbone(x_)
+        x_dsn = self.dsn_head(x[-2])
+        # mep
+        x, proj = self.ocr_asp_head(x[-1], x_dsn)
+        # mep
+        x = self.head(x)
+        x_dsn = F.interpolate(
+            x_dsn, size=(x_.size(2), x_.size(3)), mode="bilinear", align_corners=True
+        )
+        x = F.interpolate(
+            x, size=(x_.size(2), x_.size(3)), mode="bilinear", align_corners=True
+        )
+        return x_dsn, x, proj
+
+
 class RES_FCN_ASP_0_MEP(nn.Module):
 
     def __init__(self, configer):
