e1560fb601e9734222d850d80cbbb74e5f1725af
JDekun <jurry_k@outlook.com>
2023-05-31 13:13:28 +0800

    only_esay

--- segmentor/tester.py
+++ segmentor/tester.py
@@ -187,7 +187,7 @@ class Tester(object):
                 
                 # tsne
                 from lib.loss.SamplesModel.sample_tsne import Sampling
-                sample = "only_esay_one"
+                sample = "only_esay"
 
                 h, w = feats.shape[2], feats.shape[3]
                 
