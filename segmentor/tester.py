ec459500da4df66475922782b01cb733b2852a2d
JDekun <jurry_k@outlook.com>
2023-06-01 15:21:00 +0800

    de

--- segmentor/tester.py
+++ segmentor/tester.py
@@ -320,7 +320,7 @@ class Tester(object):
             )
             start = timeit.default_timer()
             outputs = self.seg_net.forward(scaled_inputs)
-            feats = outputs[2]['tsne']
+            # feats = outputs[2]['tsne']
             # feats = outputs[1]
             torch.cuda.synchronize()
             end = timeit.default_timer()
@@ -328,7 +328,7 @@ class Tester(object):
             outputs = F.interpolate(
                 outputs, size=(h, w), mode="bilinear", align_corners=True
             )
-            return outputs, feats, outputs
+            return outputs, outputs, outputs
         elif isinstance(inputs, collections.Sequence):
             device_ids = self.configer.get("gpu")
             replicas = nn.parallel.replicate(self.seg_net.module, device_ids)
